ec0e3b88657acc0bc62b462783f018f8de20225d
Jtljac <jtljac2@gmail.com>
2023-01-13 01:23:40 +0000

    Added api class for easy access to mod features

---
+++ src/main/java/com/datdeveloper/datfactions/api/FactionsAPI.java
@@ -0,0 +1,34 @@
+package com.datdeveloper.datfactions.api;
+
+import com.datdeveloper.datfactions.factionData.FLevelCollection;
+import com.datdeveloper.datfactions.factionData.FPlayerCollection;
+import com.datdeveloper.datfactions.factionData.FactionCollection;
+
+public class FactionsAPI {
+    /**
+     * Get the instance of the FactionCollection <br>
+     * This class contains the functions for managing the factions on the server
+     * @return the FactionCollection instance
+     */
+    static FactionCollection getFactionCollection() {
+        return FactionCollection.getInstance();
+    }
+
+    /**
+     * Get the instance of the FPlayerCollection <br>
+     * This class contains the functions for managing the players' faction data on the server
+     * @return the FPlayerCollection instance
+     */
+    static FPlayerCollection getPlayerCollection() {
+        return FPlayerCollection.getInstance();
+    }
+
+    /**
+     * Get the instance of the FLevelCollection <br>
+     * This class contains the function for managing the levels' faction data on the server
+     * @return the FLevelCollection instance
+     */
+    static FLevelCollection getLevelCollection() {
+        return FLevelCollection.getInstance();
+    }
+}
